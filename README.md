b896a8080ae7a32b4419d03bb343ad5d431e30f6
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-22 10:06:20 +0300

    form sync with state

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Simple form validation example
 
 we use react and Joi-browser
+
+1. sukurti registracijos forma uzdedam onSubmit eventa kuriame sustabdom issiuntima
+
+2. username, email, password, repeatPassword, agreement(checkbox )
+
+3. sinhronizuoti laukus su savo state sukurtu objektu account: {}
--- src/components/mainForm.jsx
+++ src/components/mainForm.jsx
@@ -1,11 +1,81 @@
 import React, { Component } from 'react';
 
 class MainForm extends Component {
-  state = {};
+  state = {
+    account: {
+      username: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+      agreement: '',
+    },
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Stoped form');
+  };
+
+  handleChange = (event) => {
+    console.log(event);
+    this.setState({ account: { ...this.state.account, [event.target.name]: event.target.value } });
+  };
+
+  handleCheck = (event) => {
+    console.log(event);
+    this.setState({ account: { ...this.state.account, agreement: event.target.checked } });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div className="main-form">
         <h1>Main form</h1>
+        <form onSubmit={this.handleSubmit} autoComplete="off">
+          <input
+            onChange={this.handleChange}
+            value={account.username}
+            className="input"
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
+          <input
+            onChange={this.handleChange}
+            value={account.email}
+            className="input"
+            type="text"
+            name="email"
+            placeholder="email"
+          />
+          <input
+            onChange={this.handleChange}
+            value={account.password}
+            className="input"
+            type="text"
+            name="password"
+            placeholder="password"
+          />
+          <input
+            onChange={this.handleChange}
+            value={account.repeatPassword}
+            className="input"
+            type="text"
+            name="repeatPassword"
+            placeholder="repeatPassword"
+          />
+          <div className="check-group">
+            <input
+              onChange={this.handleCheck}
+              value={account.agreement}
+              type="checkbox"
+              name="agreement"
+              id="agreement"
+            />
+            <label htmlFor="agreement">Agree?</label>
+          </div>
+          <button type="submit">Send</button>
+        </form>
       </div>
     );
   }
